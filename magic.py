5867e1ea461d5eb2c835da8721fb90c26e254d86
2nav <n-raj@outlook.com>
2023-12-01 22:21:53 +0530

    sorted ctrl rom file in `magic.py`

--- magic.py
+++ magic.py
@@ -1,11 +1,19 @@
 '''Author : Navneet Raj'''
 import csv
-g = open('rom_stuff', 'w')
-g.write('v3.0 hex words addressed\n')
-with open('Book1.csv', 'r') as f:
-    csv_reader = csv.reader(f, delimiter=',')
-    # Skip the header row
-    next(csv_reader)
-    for row in csv_reader:
-        g.write(row[4] + ': ' + row[15] + '\n')
-g.close()
+def write_rom():
+    g = open('rom_stuff', 'w')
+    g.write('v3.0 hex words addressed\n')
+    with open('Book1.csv', 'r') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        # Skip the header row
+        header = next(csv_reader)
+        data = list(csv_reader)
+        sortedlist = sorted(data, key=lambda row: int(row[-1]))
+        for row in sortedlist:
+            g.write(row[4] + ': ' + row[15] + '\n')
+    g.close()
+'''with open('Book2.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(sortedlist)
+f.close()'''
